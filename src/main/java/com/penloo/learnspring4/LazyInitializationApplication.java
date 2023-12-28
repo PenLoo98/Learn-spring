3a66d7a3f8180f9d0366f0c2bb27f35611edaf72
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-12-28 17:01:46 +0900

    feat: section 4(33~36)
    lecture 46m
    listen 215m

---
+++ src/main/java/com/penloo/learnspring4/LazyInitializationApplication.java
@@ -0,0 +1,42 @@
+package com.penloo.learnspring4;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+class ClassA{}
+@Component
+@Lazy
+class ClassB{
+    private ClassA classA;
+
+    // 즉시 초기화시 실행할 로직
+    public ClassB(ClassA classA){
+        System.out.println("Some initialization logic");
+        this.classA = classA;
+    }
+
+    public void doSomething(){
+        System.out.println("doSomething");
+    }
+}
+
+@Configuration
+@ComponentScan
+public class LazyInitializationApplication {
+    public static void main(String[] args) {
+        try(var context = new AnnotationConfigApplicationContext(LazyInitializationApplication.class)){
+//            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+            System.out.println("Initialization of context is completed.");
+            context.getBean(ClassB.class).doSomething();
+        }
+        catch (Exception exception){
+            exception.printStackTrace();
+        }
+    }
+}
