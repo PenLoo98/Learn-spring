547757092e0383a5b3450907d46c2ec5a95a3497
PenLoo98 <sohnchanho@gmail.com>
2024-06-15 17:03:30 +0900

    fix: import error in spring boot 3

--- src/main/java/com/penloo/learn4_4/CDIApplication.java
+++ src/main/java/com/penloo/learn4_4/CDIApplication.java
@@ -1,17 +1,17 @@
 package com.penloo.learn4_4;
 
-import jakarta.inject.Inject;
-import jakarta.inject.Named;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
-@Named
+@Component
 class BusinessService{
     private DataService dataService;
-    @Inject
+    @Autowired
     public void setDataService(DataService dataService) {
         System.out.println("Setter Injection");
         this.dataService = dataService;
@@ -21,7 +21,7 @@ class BusinessService{
         return dataService;
     }
 }
-@Named
+@Component
 class DataService{}
 
 @Configuration
